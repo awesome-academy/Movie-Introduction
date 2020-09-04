44ce6ee89d0a4ebe16cc3432563f6e64f342a14d
peartalent <peartalent@gmail.com>
2020-09-04 19:19:44 +0700

    [TASK][28531]: Init Project

---
+++ app/src/main/java/com/sunasterisk/movieintroduction/ui/main/GetStarted.kt
@@ -0,0 +1,23 @@
+package com.sunasterisk.movieintroduction.ui.main
+
+import android.os.Bundle
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import com.sunasterisk.movieintroduction.R
+import kotlinx.android.synthetic.main.activity_get_started.*
+
+class GetStarted : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_get_started)
+
+        val anim = AnimationUtils.loadAnimation(applicationContext, R.anim.fade_in)
+        buttonGetStarted.startAnimation(anim)
+
+        buttonGetStarted.setOnClickListener {
+            startActivity(MainActivity.getIntent(this))
+        }
+    }
+
+}
